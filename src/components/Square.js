37519b7e6ef7653afb3bd9d6b3d6ed9c1f470c04
Hasan Tareque <hasan@uxblondon.com>
2019-02-27 11:10:41 +0000

    removing control from square

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,8 +3,9 @@ import React from 'react';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" 
+        onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
